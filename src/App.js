a4e1f6a44265553084410ed77c1fbe535833a159
Lilian Xu <lilian.xu@bybit.com>
2022-02-05 14:54:45 +1300

    update menu items to navigate with Router HCO

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
+const HatsPage = () => (
+  <div>
+    <h1>Hat Page</h1>
+  </div>
+);
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </switch>
+    </div>
+  );
 }
 
 export default App;

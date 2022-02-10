2bc7138dc85bf47a986a239f9b5b192908be27ac
Lilian Xu <lilian.xu@bybit.com>
2022-02-10 16:18:01 +1300

    add header and  signin components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 const HatsPage = () => (
   <div>
     <h1>Hat Page</h1>
@@ -11,9 +13,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
